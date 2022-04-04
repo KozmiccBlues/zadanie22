a7c6e566f156aa4a580b6a32eabf484f5022be5c
Ola <kozlowska.aleksandra92@gmail.com>
2022-04-04 19:44:48 +0200

    Stworzenie projektu

---
+++ src/test/java/pl/akoz/zadanie22/Zadanie22ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akoz.zadanie22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
